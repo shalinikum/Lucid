c6c2f165fc39554dea7f88add733b997c5e71ba4
shalinikum <shalinik@iitbhilai.ac.in>
2021-05-13 17:40:06 +0530

    login and registration done

--- registration/views.py
+++ registration/views.py
@@ -4,7 +4,29 @@ from django.contrib.auth.models import auth
 from django.db import models
 from practice.models import User
 import datetime
+from django.contrib import messages
 from django.utils import timezone
+def login(request):
+    if request.method== 'POST':
+        user_name = request.POST['user_name']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(user_name= user_name)
+            if user.password == password:
+                return redirect("/practice")
+            else:
+                messages.info(request,'invalid credentials')
+                return redirect('login')
+        except User.DoesNotExist:
+            messages.info(request,'invalid user_name')
+            return redirect('login')
+
+    
+
+    else:
+        return render(request,'login.html')
+
 def register(request):
 
     if request.method == "POST":
@@ -17,11 +39,26 @@ def register(request):
         password2 =  request.POST['password2']
         age = int(request.POST['age'])
         user_name = request.POST['user_name']
-    
-        user = User(name=name,email=email,gender=gender, therapist_id=therapist,phone_number=phone_number, password=password1, age=age, user_name=user_name,joining_date=timezone.now())
-        user.save();
-        print('user created')
-        return redirect('/practice')
+
+
+        if password1==password2:
+            if User.objects.filter(user_name=user_name).exists():
+                messages.info(request,'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email Taken')
+                return redirect('register')
+            else:   
+                user = User(name=name,email=email,gender=gender, therapist_id=therapist,phone_number=phone_number, password=password1, age=age, user_name=user_name,joining_date=timezone.now())
+                user.save()
+                print('user created')
+                return redirect('/practice')
+                
+
+        else:
+            messages.info(request,'password not matching..')    
+            return redirect('register')
+
     else:
         return render(request,'register.html')
 
